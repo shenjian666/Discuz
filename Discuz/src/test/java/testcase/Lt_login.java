b59485cff98cfe69fa6efc00387b22431bf563b9
shenjian666 <857898071@qq.com>
2018-05-30 16:42:41 +0800

    haha

--- Discuz/src/test/java/testcase/Lt_login.java
+++ Discuz/src/test/java/testcase/Lt_login.java
@@ -1,4 +1,4 @@
-﻿package testcase;
+package testcase;
 
 import org.testng.annotations.Test;
 import pageHelper.HelperLinkTextPage;
@@ -15,7 +15,6 @@ public class Lt_login extends BasicTestCase{
       HelperLoginPage.login(seleniumUtil,"test","123456");
      Thread.sleep(3000);
     HelperLoginPage.ft(seleniumUtil,"Hello World","大家好,哈哈哈哈哈哈哈哈");
-
 
     HelperReplyPage.reply(seleniumUtil,"哇，你这个逗比");
 
